f96cde4d879f3a56835f3bc793c0b4f29fadb7a4
shivanand1 <96418666+shivanandsc@users.noreply.github.com>
2022-04-03 04:14:04 +0000

    Book table configured

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import random
 
 app = Flask(__name__)
 
-global Api_key
-Api_key='bdy6etf4yegf3cf3' 
+
 ##Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -22,15 +21,27 @@ class Author(db.Model):
     Death_Date = db.Column(db.String(250), nullable=True)
   
     def to_dict(self):
-        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+        return {column.name: getattr(self, column.Name) for column in self.__table__.columns}
+
+
+##Book TABLE Configuration
+class Book(db.Model):
+    Book_Id = db.Column(db.Integer, primary_key=True)
+    Title = db.Column(db.String(250), unique=True, nullable=False)
+    Author_id = db.Column(db.String(500), nullable=False)
+    Publisher = db.Column(db.String(500), nullable=True)
+    categorya_Id = db.Column(db.String(250), nullable=False)
+    Price=db.Column(db.String(500), nullable=False)
+    Sold_Count=db.Column(db.String(500), nullable=True)
+  
+    def to_dict(self):
+        return {column.name: getattr(self, column.Title) for column in self.__table__.columns}
+
 
 @app.route("/")
 def home():
     return 'hi'
 
-@app.route("/home")
-def new():
-    return 'home'
 
 
 
